875bf95c17145791710bf69c8bac73618b10227c
TobyXYan <45277895+TobyXYan@users.noreply.github.com>
2020-11-18 08:49:47 +0800

    Add files via upload

---
+++ MVVMUnaidded/Models/ItemNode.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVMUnaidded.Models
+{
+    public class ItemNode : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        /// <summary>
+        /// Notifies listeners that a property value has changed.
+        /// </summary>
+        /// <param name="propertyName">Name of the property used to notify listeners.  This
+        /// value is optional and can be provided automatically when invoked from compilers
+        /// that support <see cref="CallerMemberNameAttribute"/>.</param>
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            var eventHandler = this.PropertyChanged;
+            if (eventHandler != null)
+                eventHandler(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private ObservableCollection<ItemNode> _children = new ObservableCollection<ItemNode>();
+
+        public ObservableCollection<ItemNode> Children
+        {
+            get { return _children; }
+            set { _children = value;  OnPropertyChanged(nameof(Children)); }
+        }
+
+        private string _displayName = string.Empty;
+
+        public string DisplayName
+        {
+            get { return _displayName; }
+            set { _displayName =  value; OnPropertyChanged(nameof(DisplayName)); }
+        }
+
+
+    }
+}
